c6c3ea9e99a0447f5770c2ce6d2085b607be6ba0
Luke Starnes <luke.starnes@gtri.gatech.edu>
2020-03-15 18:24:09 -0400

    file cleanup

--- app.py
+++ app.py
@@ -6,19 +6,21 @@ import pandas as pd
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 who_data = pd.read_csv('https://covid.ourworldindata.org/data/full_data.csv')
 t0_threshold = 100
+
 who_data_t0 = who_data.query('total_cases >= @t0_threshold')
+who_data_t0['date'] = pd.to_datetime(who_data_t0['date'])
+
 t0_date = who_data_t0.groupby('location').min()['date']
 who_data_t0.loc[:, 't0_date'] = who_data_t0['location'].map(t0_date)
-who_data_t0.loc[:, 'since_t0'] = pd.to_datetime(who_data_t0['date']) - pd.to_datetime(who_data_t0['t0_date'])
+who_data_t0.loc[:, 'since_t0'] = who_data_t0['date'] - who_data_t0['t0_date']
 who_data_t0.loc[:, 'since_t0']  = who_data_t0['since_t0'].map(lambda x: x.days)
-who_data_t0['date'] = pd.to_datetime(who_data_t0['date'])
 
 country_filter = ['China', 'South Korea', 'Italy', 'Iran', 'United States']
 
-server = app.server
 dcc.Graph.responsive = True
 
 x_max = min(who_data_t0[who_data_t0['location'] == 'United States']['since_t0'].max() + 14, 
