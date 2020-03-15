56df9beda927dc8b2d6535d6a429edd35d66ee51
Luke Starnes <luke.starnes@gtri.gatech.edu>
2020-03-15 16:57:17 -0400

    FIX: defined server var

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
 
+server = app.server
 
 app.layout = html.Div([
     dcc.Graph(
