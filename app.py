8bfc683c813e89088d2b9bfb4986f1077803c4ae
Luke Starnes <luke.starnes@gtri.gatech.edu>
2020-03-16 11:46:40 -0400

    updated to use JHU data

--- app.py
+++ app.py
@@ -13,23 +13,39 @@ who_data = pd.read_csv('https://covid.ourworldindata.org/data/full_data.csv')
 jhu_data = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/'+
                        'COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/'+
                        'time_series_19-covid-Confirmed.csv')
+
 t0_threshold = 100
-
-who_data_t0 = who_data.query('total_cases >= @t0_threshold')
-who_data_t0['date'] = pd.to_datetime(who_data_t0['date'])
-
-t0_date = who_data_t0.groupby('location').min()['date']
-who_data_t0.loc[:, 't0_date'] = who_data_t0['location'].map(t0_date)
-who_data_t0.loc[:, 'since_t0'] = who_data_t0['date'] - who_data_t0['t0_date']
-who_data_t0.loc[:, 'since_t0']  = who_data_t0['since_t0'].map(lambda x: x.days)
-
 country_filter = ['China', 'South Korea', 'Italy', 'Iran', 'United States']
-countries = list(set(who_data_t0.sort_values(by='total_cases', ascending=False)['location']))
+country_mapper = {
+        'Korea, South': 'South Korea',
+        'US': 'United States'    
+}
+
+jhu_data['Country/Region'] = jhu_data['Country/Region'].map(country_mapper).fillna(jhu_data['Country/Region'])
+assert set(country_filter) - set(jhu_data['Country/Region']) == set()
+
+jhu_data_reduced = jhu_data.drop(columns=['Lat', 'Long'])
+jhu_data_reduced = jhu_data_reduced.groupby('Country/Region').sum()
+jhu_data_reduced = jhu_data_reduced.stack().reset_index()
+jhu_data_reduced.columns = ['location', 'date', 'total_cases']
+jhu_data_reduced['date'] = pd.to_datetime(jhu_data_reduced['date'])
+
+
+
+jhu_data_t0 = jhu_data_reduced.query('total_cases >= @t0_threshold')
+
+t0_date = jhu_data_t0.groupby('location').min()['date']
+jhu_data_t0.loc[:, 't0_date'] = jhu_data_t0['location'].map(t0_date)
+jhu_data_t0.loc[:, 'since_t0'] = jhu_data_t0['date'] - jhu_data_t0['t0_date']
+jhu_data_t0.loc[:, 'since_t0']  = jhu_data_t0['since_t0'].map(lambda x: x.days)
+jhu_data_t0.loc[:, 'since_t0'] = jhu_data_t0.loc[:, 'since_t0'].where(jhu_data_t0['since_t0'] > 0, 0)
+
+countries = list(set(jhu_data_t0.sort_values(by='total_cases', ascending=False)['location']))
 
 dcc.Graph.responsive = True
 
-x_max = min(who_data_t0[who_data_t0['location'] == 'United States']['since_t0'].max() + 14, 
-            who_data_t0['since_t0'].max()) 
+x_max = min(jhu_data_t0[jhu_data_t0['location'] == 'United States']['since_t0'].max() + 14, 
+            jhu_data_t0['since_t0'].max()) 
 
 
 app.layout = html.Div([
@@ -41,9 +57,9 @@ app.layout = html.Div([
                 figure={
                     'data': [
                         dict(
-                            x=who_data_t0[who_data_t0['location'] == i]['since_t0'],
-                            y=who_data_t0[who_data_t0['location'] == i]['total_cases'],
-                            text=who_data_t0[who_data_t0['location'] == i]['date'].map(lambda x: f'{x:%m-%d-%Y}'),
+                            x=jhu_data_t0[jhu_data_t0['location'] == i]['since_t0'],
+                            y=jhu_data_t0[jhu_data_t0['location'] == i]['total_cases'],
+                            text=jhu_data_t0[jhu_data_t0['location'] == i]['date'].map(lambda x: f'{x:%m-%d-%Y}'),
                             mode='lines',
                             opacity=0.7,
                             marker={
@@ -57,7 +73,7 @@ app.layout = html.Div([
                         ) for i in country_filter
                     ],
                     'layout': dict(
-                        xaxis={'title': 'Days Since Cases = 100', 'range': [0, x_max]},
+                        xaxis={'title': 'Days Since Cases = 100', 'range': [0, x_max], 'zeroline': False},
                         yaxis={'type': 'log', 'title': 'Total Confirmed Cases'},
                         margin={'l': 100, 'b': 40, 't': 10, 'r': 10},
                         hovermode='compare'
@@ -71,10 +87,10 @@ app.layout = html.Div([
                 figure={
                     'data': [
                         dict(
-                            x=who_data_t0[who_data_t0['location'] == i]['since_t0'],
-                            y=who_data_t0[who_data_t0['location'] == i]['total_cases'],
+                            x=jhu_data_t0[jhu_data_t0['location'] == i]['since_t0'],
+                            y=jhu_data_t0[jhu_data_t0['location'] == i]['total_cases'],
                             name=i,
-                            text=who_data_t0[who_data_t0['location'] == i]['date'].map(lambda x: f'{x:%m-%d-%Y}'),
+                            text=jhu_data_t0[jhu_data_t0['location'] == i]['date'].map(lambda x: f'{x:%m-%d-%Y}'),
                             type='bar',
                             opacity=0.7,
                             hovertemplate='%{text} (Day %{x})<br>'
@@ -93,8 +109,8 @@ app.layout = html.Div([
         ])
     ]),
         html.P(['data source: ',
-                html.A("https://ourworldindata.org/coronavirus-source-data", 
-                       href="https://ourworldindata.org/coronavirus-source-data"), 
+                html.A("https://github.com/CSSEGISandData/COVID-19", 
+                       href="https://github.com/CSSEGISandData/COVID-19"), 
                 html.Br(), 
                 'code source: ',
                 html.A("https://github.com/slstarnes/coronavirus-stats", 
