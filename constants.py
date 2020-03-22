39ffbace99cf388f395b1d4a6386b6094a320d8e
Luke Starnes <luke.starnes@gtri.gatech.edu>
2020-03-22 00:56:02 -0400

    added country selector

--- constants.py
+++ constants.py
@@ -7,6 +7,6 @@ JHU_DATA = 'https://raw.githubusercontent.com/CSSEGISandData/' \
 JHU_DATA_FILE_URL = 'https://github.com/CSSEGISandData/COVID-19/blob/master/' \
                      'csse_covid_19_data/csse_covid_19_time_series/' \
                      'time_series_19-covid-Confirmed.csv'
-PLOT_LOOKAHEAD = 14  # days
+PLOT_LOOKAHEAD = 7  # days
 TRACE_COLORS = ["#4C72B0", "#DD8452", "#55A868", "#C44E52", "#8172B3",
                 "#937860", "#DA8BC3", "#8C8C8C", "#CCB974", "#64B5CD"]
