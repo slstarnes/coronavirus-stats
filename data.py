a02d4ffd6680289e69ddeed2e5a2a8372a7c3da4
Luke Starnes <luke.starnes@gtri.gatech.edu>
2020-03-26 00:00:26 -0400

    removed commented out code

--- data.py
+++ data.py
@@ -11,6 +11,7 @@ country_mapper = {
         'Korea, South': 'South Korea',
         'US': 'United States'    
 }
+state_filter = ['New York', 'Washington', 'Ohio', 'Georgia']
 
 
 def _drop_cities(place):
@@ -22,52 +23,43 @@ def _drop_cities(place):
         return place
 
 
-data['Country/Region'] = data['Country/Region'].map(country_mapper).fillna(data['Country/Region'])
-assert set(country_filter) - set(data['Country/Region']) == set()
+def data_processing(df, pop_dict, t0_threshold=100,
+                    population_group_size=100_000, states_data=False):
+    loc = 'location' if not states_data else 'state'
+    df['Country/Region'] = df['Country/Region'].map(country_mapper).fillna(df['Country/Region'])
+    if states_data:
+        df = df[df['Country/Region'] == 'United States']
+        df['Province/State'] = df['Province/State'].map(_drop_cities).dropna()
+        df = df.drop(columns=['Lat', 'Long', 'Country/Region'])
+        df = df.groupby('Province/State').max()
+    else:
+        df = df.drop(columns=['Lat', 'Long', 'Province/State'])
+        df = df.groupby('Country/Region').max()
+    df = df.stack().reset_index()
+    df.columns = [loc, 'date', 'total']
+    df['date'] = pd.to_datetime(df['date'])
+    df['population'] = df[loc].map(pop_dict).fillna(1).astype(int)
+    df['per_100k'] = (df['total'].fillna(0).astype(int)
+                      .div(df['population']).mul(population_group_size))
+    df = df.query('total >= @t0_threshold')
+    t0_date = df.groupby(loc).min()['date']
+    df.loc[:, 't0_date'] = pd.to_datetime(df[loc].map(t0_date))
+    df.loc[:, 'since_t0'] = df['date'] - df['t0_date']
+    df.loc[:, 'since_t0'] = df['since_t0'].map(lambda x: x.days)
+    df.loc[:, 'since_t0'] = df.loc[:, 'since_t0'].where(df['since_t0'] > 0, 0)
+    return df
 
-data_reduced = data.drop(columns=['Lat', 'Long'])
-data_reduced = data_reduced.groupby('Country/Region').sum()
-data_reduced = data_reduced.stack().reset_index()
-data_reduced.columns = ['location', 'date', 'total_cases']
-data_reduced['date'] = pd.to_datetime(data_reduced['date'])
 
-data_reduced['population'] = data_reduced['location'].map(population_dict).fillna(1).astype(int)
-data_reduced['cases_per_100k'] = (data_reduced['total_cases'].fillna(0).astype(int)
-                                  .div(data_reduced['population']).mul(100_000))
-
-data_t0 = data_reduced.query('total_cases >= @t0_threshold')
-
-t0_date = data_t0.groupby('location').min()['date']
-data_t0.loc[:, 't0_date'] = data_t0['location'].map(t0_date)
-data_t0.loc[:, 'since_t0'] = data_t0['date'] - data_t0['t0_date']
-data_t0.loc[:, 'since_t0'] = data_t0['since_t0'].map(lambda x: x.days)
-data_t0.loc[:, 'since_t0'] = data_t0.loc[:, 'since_t0'].where(data_t0['since_t0'] > 0, 0)
-
-state_filter = ['New York', 'Washington', 'Ohio', 'Georgia']
-
-data_us = data[data['Country/Region'] == 'United States']
-data_us['Province/State'] = data_us['Province/State'].map(_drop_cities).dropna()
-data_us_reduced = data_us.drop(columns=['Lat', 'Long', 'Country/Region'])
-data_us_reduced = data_us_reduced.groupby('Province/State').max()
-data_us_reduced = data_us_reduced.stack().reset_index()
-data_us_reduced.columns = ['state', 'date', 'total_cases']
-data_us_reduced['date'] = pd.to_datetime(data_us_reduced['date'])
-
-data_us_reduced['population'] = data_us_reduced['state'].map(us_population_dict).fillna(1).astype(int)
-data_us_reduced['cases_per_100k'] = (data_us_reduced['total_cases'].fillna(0).astype(int)
-                                     .div(data_us_reduced['population']).mul(100_000))
-
-data_us_t0 = data_us_reduced.query('total_cases >= 1')
-
-t0_date_us = data_us_t0.groupby('state').min()['date']
-data_us_t0.loc[:, 't0_date'] = data_us_t0['state'].map(t0_date_us)
-data_us_t0.loc[:, 'since_t0'] = pd.to_datetime(data_us_t0['date']) - pd.to_datetime(data_us_t0['t0_date'])
-data_us_t0.loc[:, 'since_t0'] = data_us_t0['since_t0'].map(lambda x: x.days)
-data_us_t0.loc[:, 'since_t0'] = data_us_t0.loc[:, 'since_t0'].where(data_us_t0['since_t0'] > 0, 0)
+data_t0 = data_processing(data, population_dict,
+                          t0_threshold=100)
+data_us_t0 = data_processing(data, us_population_dict,
+                             t0_threshold=1, states_data=True)
 
 
 def get_data(data_set='country'):
     if data_set == 'country':
+        # data_t0.to_csv('data_t0_2.csv')
         return data_t0
     elif data_set == 'state':
+        # data_us_t0.to_csv('data_us_t0_2.csv')
         return data_us_t0
