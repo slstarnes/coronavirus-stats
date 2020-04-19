949546c0dcf1c6bcf77e01ce6c1fd720ec7d58a5
Luke Starnes <luke.starnes@gtri.gatech.edu>
2020-04-19 14:59:24 -0400

    FIX: fixed parse of github page + added error handling

--- data_mod_date.py
+++ data_mod_date.py
@@ -7,13 +7,21 @@ import pytz
 def get_data_mod_date(url, datetime=False):
     r = requests.get(url)
     if r.status_code == 200:
-        tree = html_parser.fromstring(r.content)
-        t = tree.xpath('/html/body/div[4]/div/main/div[2]/div/div[3]/div[1]/span[2]/relative-time')
-        d = date_parser(t[0].attrib['datetime'])
-        tz = pytz.timezone('America/New_York')
-        d = d.astimezone(tz)
+        try:
+            tree = html_parser.fromstring(r.content)
+            t = tree.xpath('/html/body/div[4]/div/main/div[2]/div/div[2]/div[1]/span[2]/relative-time')
+            d = date_parser(t[0].attrib['datetime'])
+            tz = pytz.timezone('America/New_York')
+            d = d.astimezone(tz)
+        except:
+            d = ''
         if datetime:
             return d
         else:
-            return f'{d:%m-%d-%Y @ %-I:%M %p %Z}'
+            try:
+                return f'{d:%m-%d-%Y @ %-I:%M %p %Z}'
+            except ValueError:
+                print(f'Unable to parse mod date for {url}')
+                return d
+
     return
